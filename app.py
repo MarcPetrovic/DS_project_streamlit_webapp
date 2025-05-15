2fa4fab27aeeab9c793535dab9250debf25b803d
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-05-15 20:23:24 +0200

    Update navigation app.py

--- app.py
+++ app.py
@@ -3,22 +3,42 @@ import streamlit as st
 # Erstelle eine Sidebar mit einem Selectbox-Menü für die Navigation
 navigation = st.sidebar.radio(
     "Navigation",
-    ("Problem Statement", "Methods & Data", "Technical Environment & Modeling", "Key Results", "Conclusion & Discussion")
+    ("Introduction", "Theoretical framework", "Methods & Data", "Technical Environment & Modeling", "Key Results", "Conclusion & Discussion")
 )
 
+
 # Basierend auf der Auswahl des Benutzers den Inhalt anpassen
-if navigation == "Problem Statement":
-    st.title("Problem Statement")
+if navigation == "Introduction":
+    st.title("Introduction")
     st.markdown("""
-    ## Hintergrund
-    Hier kannst du das Problem in mehreren Abschnitten erläutern. Zum Beispiel:
-    
-    - Was ist das Problem?
-    - Warum ist es wichtig?
-    - Wer ist betroffen?
+    Over the last two decades, globalization and ever-increasing regulation by the banking supervisory 
+    authorities have presented the financial sector with major challenges: On the one hand, open markets 
+    have led to an intensification of competitive pressure, which has simultaneously led to a decline in 
+    profit margins. On the other hand, financial service providers had to implement cross-border requirements 
+    to protect their customers and secure their own liquidity, the implementation of which was associated 
+    with additional financial costs. Banks could and can only withstand the economic pressure described 
+    above if they see digitalization as an opportunity. Modern marketing management at financial service 
+    providers therefore combines business and artificial intelligence concepts with machine learning methods 
+    (e.g. decision trees, regression or dimension reduction) in order to generate cost-opti¬mized 
+    communication campaigns.
     
     ## Ziel der Untersuchung
     Dein Ziel könnte sein, Lösungen zu finden, die helfen, dieses Problem zu adressieren.
+    """)
+    # Weitere Details zum Problem Statement hinzufügen 
+# Basierend auf der Auswahl des Benutzers den Inhalt anpassen
+elif navigation == "Theoretical framework":
+    st.title("Problem Statement")
+    st.markdown("""
+    ## PORTER’S VALUE CHAIN APPROACH AND COST OPTIMIZATION
+    The general institutionalization of Porter's value chain approach at the beginning of the 1980s 
+    also led to a shift away from traditional sales models in companies. The special feature of Porter's 
+    approach is that the division between primary and secondary activities within the value chain and 
+    the comparison of these with the margin makes it possible to determine the costs of producing a 
+    product or service. Through the practical application of Porter's value chain approach, it has been 
+    empirically proven that conventional sales strategies, in which all customers are offered the same 
+    sales promotion and at the same time the differences between customers are neglected, are associated 
+    with high operating expenses and low monetary benefits
     """)
     # Weitere Details zum Problem Statement hinzufügen
 
