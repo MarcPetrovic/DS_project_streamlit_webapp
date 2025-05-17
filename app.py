1d887b9dc7a3435ef8e794dcbc69e9dfa6b8a6a3
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-05-17 20:34:59 +0200

    Update app.py
    changed folder name page to my_page

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ main_page = st.sidebar.radio("Navigation", [
 
 # Introduction
 if main_page == "Introduction":
-    from pages import introduction
+    from my_pages import introduction
     introduction.show()
 
 # Theoretical Framework + Subnavigation
 elif main_page == "Theoretical Framework":
-    from pages import theoretical_framework_overview
+    from my_pages import theoretical_framework_overview
     theoretical_framework_overview.show()
 
     sub_section = st.sidebar.radio("Subtopics", [
@@ -28,37 +28,37 @@ elif main_page == "Theoretical Framework":
     ])
 
     if sub_section == "PORTER'S VALUE CHAIN APPROACH AND COST OPTIMIZATION":
-        from pages.theoretical_framework import porters_value_chain
+        from my_pages.theoretical_framework import porters_value_chain
         porters_value_chain.show()
 
     elif sub_section == "THE RELEVANCE OF DATA MINING FOR DIRECT MARKETING CAMPAIGNS":
-        from pages.theoretical_framework import relevance_data_mining
+        from my_pages.theoretical_framework import relevance_data_mining
         relevance_data_mining.show()
 
     elif sub_section == "SUPERVISED LEARNING AND THE RESPONSE-MODEL":
-        from pages.theoretical_framework import supervised_learning
+        from my_pages.theoretical_framework import supervised_learning
         supervised_learning.show()
 
     elif sub_section == "BRIEF INTRODUCTION TO CRISP-DM":
-        from pages.theoretical_framework import introduction_crisp_dm
+        from my_pages.theoretical_framework import introduction_crisp_dm
         introduction_crisp_dm.show()
 
 # Methods & Data
 elif main_page == "Methods & Data":
-    from pages import methods_data
+    from my_pages import methods_data
     methods_data.show()
 
 # Technical Environment & Modeling
 elif main_page == "Technical Environment & Modeling":
-    from pages import technical_environment_modeling
+    from my_pages import technical_environment_modeling
     technical_environment_modeling.show()
 
 # Key Results
 elif main_page == "Key Results":
-    from pages import key_results
+    from my_pages import key_results
     key_results.show()
 
 # Conclusion & Discussion
 elif main_page == "Conclusion & Discussion":
-    from pages import conclusion_discussion
+    from my_pages import conclusion_discussion
     conclusion_discussion.show()
