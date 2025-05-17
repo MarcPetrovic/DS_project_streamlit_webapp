0125ed9459e8d5add741151b9e0920962835b6f7
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-05-17 19:18:35 +0200

    Update porters_value_chain.py
    letter correction

--- pages/theoretical_framework/porters_value_chain.py
+++ pages/theoretical_framework/porters_value_chain.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def show():
-    st.header("Porter’s Value Chain Approach and Cost Optimization")
+    st.header("Porter's Value Chain Approach and Cost Optimization")
     st.write("Hier steht dein Inhalt zur Porter's Value Chain...")
