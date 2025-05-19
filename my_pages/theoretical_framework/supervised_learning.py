235a74f600b0c3428e53ca28d18b466da696d940
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-05-19 22:04:42 +0200

    Update supervised_learning.py
    inserted paragraphes

--- my_pages/theoretical_framework/supervised_learning.py
+++ my_pages/theoretical_framework/supervised_learning.py
@@ -1,5 +1,27 @@
 import streamlit as st
 
 def show():
-    st.header("Supervised Learning")
-    st.write("Hier beschreibst du Maschinelles Lernen.")
+    st.header("Supervised Learning and the Resonse model")
+    st.markdown("""
+    The response model is a well-known technique often used by direct marketing analysts and can be categorized 
+    as a supervised learning method. A response model predicts the likelihood that a customer will respond to 
+    a promotion or offer. Response models are usually created on the basis of historical purchase data. The 
+    model allows companies to identify a subset of customers who are more likely to respond than other customer 
+    sup-groups. The main purpose of these models is to select the customers who are most interested in a 
+    particular product offering so that the largest possible percentage of target customers respond positively 
+    to the product offering. In this way, companies can maximize their profits when selling products or services 
+    and minimize the costs of the marketing campaign. In addition, the use of data mining techniques improves 
+    return on investment, customer relationships and customer loyalty.
+
+    The response model is usually a binary classification problem. Customers are divided into two classes, e.g. 
+    respondents and non-respondents. Various classification methods are used for response modelling, e.g. 
+    statistical and machine learning methods such as neural networks or other classification algorithms. All 
+    data pattern recognition methods, including the response model, can be viewed as a process that proceeds in 
+    stages from problem formulation, data acquisition, data pre-processing, feature construction, feature 
+    selection, class matching, actual algorithmic pattern recognition and interpretation (modelling and evaluation) 
+    to model implementation. Once the model has been introduced and is in use, adjustments are usually necessary 
+    again and again, which leads to a cyclical restart of the process. The procedure described corresponds to 
+    the generally recognized and widely used industry standard CRISP-DM (‘Cross-industry Standard Process for 
+    Data Mining’).
+
+    """)
