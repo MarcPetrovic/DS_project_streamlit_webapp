20e35f92ba08a327eeed10bf6ed5e98abe2376c2
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-05-19 21:50:14 +0200

    Update introduction.py
    inserting further paragraphes

--- my_pages/introduction.py
+++ my_pages/introduction.py
@@ -25,6 +25,27 @@ def show():
     (e.g. decision trees, regression or dimension reduction) in order to generate cost-optimized 
     communication campaigns.
     
-    ## Ziel der Untersuchung
-    Dein Ziel könnte sein, Lösungen zu finden, die helfen, dieses Problem zu adressieren.
+    This report describes the implementation of an AI project based on the CRISP-DM methodology. The used data 
+    were collected by a Portuguese bank that used its own contact centre for targeted marketing campaigns. The 
+    bank customers were contacted by telephone with human agents as dialogue partners and received an attractive 
+    offer for long-term deposits with good interest rates (so-called fixed-term deposits). The dataset consists 
+    of 17 telephone campaigns conducted between May 2008 and November 2010, totaling 41,188 contacts. A large 
+    number of characteristics were recorded for each contact (see CRISP PHASE II - DATA STATEMENT in detail) 
+    and also whether the offer (conclusion of a fixed-term deposit contract, the target variable) was accepted 
+    by the contacted customer. The total data sample comprises 4,640 contracts, which corresponds to a success 
+    rate of 11.3 %. 
+
+    The central aim of the project is to develop a statistically relevant (response) model that can explain the 
+    success of a telephone contact (described model from the field of business intelligence). Such a response 
+    model should increase the efficiency of a campaign by identifying the most important characteristics that 
+    influence success and contribute to the optimal use of available resources (e.g. working time and number of 
+    telephone calls). Essentially, the generated model should produce cost-optimized results in the segmentation 
+    of a high-quality group of potential customers (prediction model as a methodology of basic machine learning).
+
+    The project report is organized as follows: Sect. 2 provides some theoretical background on Data Mining, 
+    Machine Learning, and their applications in Marketing. Section 3 illustrates the methodology adopted for 
+    covering the Data Mining practical project. Section 4 presents the findings and describes in detail the pros 
+    and cons of the selected model. Finally, the last section discusses conclusions and acknowledges the 
+    limitations of the study, highlighting opportunities for further approaches.
+    
     """)
