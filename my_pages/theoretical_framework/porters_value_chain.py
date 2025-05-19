dc3481458e1d2f2c54a65f054eaadd72620a6506
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-05-19 21:55:45 +0200

    Update porters_value_chain.py
    inserting paragraphes

--- my_pages/theoretical_framework/porters_value_chain.py
+++ my_pages/theoretical_framework/porters_value_chain.py
@@ -2,4 +2,32 @@ import streamlit as st
 
 def show():
     st.header("Porter's Value Chain Approach and Cost Optimization")
-    st.write("Hier steht dein Inhalt zur Porter's Value Chain...")
+    st.markdown("""
+    The general institutionalization of Porter's value chain approach at the beginning of the 1980s also led 
+    to a shift away from traditional sales models in companies. The special feature of Porter's approach is 
+    that the division between primary and secondary activities within the value chain and the comparison of 
+    these with the margin makes it possible to determine the costs of producing a product or service. Through 
+    the practical application of Porter's value chain approach, it has been empirically proven that conventional 
+    sales strategies, in which all customers are offered the same sales promotion and at the same time the 
+    differences between customers are neglected, are associated with high operating expenses and low monetary 
+    benefits.
+
+    In addition to the process optimization described above, which focused on resource-optimized use, other 
+    macroeconomic aspects, such as the creation of new retail relationships and the associated emergence of 
+    so-called global markets, increased the pressure between competitors within specific sectors. Ultimately, 
+    however, it was digitalization and the option of omnichannel marketing in particular that caused a fundamental 
+    break within the corporate culture. Within this new culture, the econo¬mics of customer relationships changed 
+    fundamentally and companies implemented new concepts of customer relations. The primary marketing objective 
+    changed in the way that the focus was no longer on the cost-intensive expansion of the customer base, but 
+    rather on intensifying new business with existing customers (cross-selling and up-selling).
+
+    Over the last two decades, the focus of marketing has shifted from the breadth of the customer base to the 
+    depth of the needs of individual customers. Today, successful companies are practising the new economics of 
+    the customer relationship by being interested in long-term customer relationships when the customer value is 
+    in favour. Every interaction with profitable customers is aimed at providing them with a service experience 
+    and building and maintaining a direct relationship. The latter primarily serves to gather information about 
+    the customer in order to create needs-based product offerings (user-centred product development) and to find 
+    the right time to contact the customer (best time to contact).
+
+    
+    """)
