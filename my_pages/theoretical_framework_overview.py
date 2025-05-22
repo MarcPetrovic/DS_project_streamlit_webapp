b992c506122151420cc4376d6517fbf1db84dac0
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-05-22 19:08:57 +0200

    Update theoretical_framework_overview.py
    deleted markdown

--- my_pages/theoretical_framework_overview.py
+++ my_pages/theoretical_framework_overview.py
@@ -2,6 +2,3 @@ import streamlit as st
 
 def show():
     st.header("Theoretical Framework")
-    st.markdown("""
-    The response model is a well-known technique often used by direct marketing analysts and can be categorized
-    """)
