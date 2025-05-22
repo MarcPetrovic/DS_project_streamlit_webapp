2f51ad0b440e29e91e2208f2f07c34b106ec7a6d
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-05-22 21:24:53 +0200

    Create data_understanding.py
    initial

---
+++ my_pages/practical_application/data_understanding.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from utils.image_loader import show_github_image
+
+def show():
+    st.header("Data Understanding")
+    st.markdown("""
+    A growing number of companies, especially financial service providers, banks and insurance companies, are 
+    using customer relationship management with the sole aim of customer profiling. Recent studies clearly 
+    show that there is not only a linear relationship between marketing expenditure and profit margins per 
+    customer, but that a competitive advantage can be achieved through the systematic use of modern methods 
+    of data pattern recognition, such as data mining and knowledge discovery in databases (KDD). Data mining 
+    refers to the examination and analysis of large amounts of data in order to discover meaningful patterns 
+    and rules. Direct marketing has developed into an important area of application for data mining, 
+    particularly in connection with customer targeting, so that a taxonomy for strategic use in marketing 
+    management has now become established (see Figure 3). 
+    """)
