9ad69cfc4aaad59eb2457cdec80e0b1978183b6f
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-05-22 20:54:13 +0200

    Update practical_application_crisp_dm.py
    testab

--- my_pages/practical_application_crisp_dm.py
+++ my_pages/practical_application_crisp_dm.py
@@ -1,15 +1,4 @@
 import streamlit as st
-from utils.image_loader import show_github_image
 
 def show():
-    st.header("Introduction")
-    st.markdown("""
-    <hi style='font-size: 32px; font-weight: bold;'>AI for bank marketing campaigns – <br>A data-driven path to cost-optimized direct customer contact in term deposit sales business</hi>
-    """, unsafe_allow_html=True)
-    
-    # Bild von der URL laden und anzeigen
-    show_github_image(
-    image_filename="images/porter.PNG",
-    repo_url="https://github.com/MarcPetrovic/DS_project_streamlit_webapp",
-    caption="Porter's Value Chain"
-    )
+    st.write("📢 practical_application_crisp_dm.show() wurde aufgerufen")
