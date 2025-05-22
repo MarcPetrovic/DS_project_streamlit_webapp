b3ad12428d26f3efe22da7f55748f1153d34e1e7
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-05-22 21:08:17 +0200

    Update practical_application_crisp_dm.py
    test

--- my_pages/practical_application_crisp_dm.py
+++ my_pages/practical_application_crisp_dm.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 def show():
     st.header("Practical Application According to CRISP-DM")
+    st.markdown("""
+    The following chapter will goes through the first 5 phases of CRISP-DM in detail, with the key results 
+    from each phase listed in Figure 6. Each of the following sections is structured in such a way that the 
+    core results are listed at the end. This is based on the hierarchical CRISP-DM levels (phases, generic 
+    tasks, special activities) described in Chapter 2 of this research paper
+    """)
