681d6a5c8e1d8842a7fb5ff7e38235ef9808db83
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-06-20 19:53:08 +0200

    Delete utils directory
    redundant

--- utils/profiling_viewer.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def show_profiling_report(html_path: str):
-    try:
-        with open(html_path, 'r', encoding='utf-8') as f:
-            html_content = f.read()
-            st.components.v1.html(html_content, height=1000, scrolling=True)
-    except FileNotFoundError:
-        st.error(f"Profiling-Report nicht gefunden unter: {html_path}")
