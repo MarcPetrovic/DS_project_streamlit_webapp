e4e27add62fc8e71493dfc4879ddef0ca4add0da
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-06-21 19:56:10 +0200

    Update evaluation.py

--- my_pages/practical_application/evaluation.py
+++ my_pages/practical_application/evaluation.py
@@ -10,6 +10,8 @@ from sklearn import set_config
 from sklearn.utils import estimator_html_repr
 from utils.summary_stats import summary
 from utils.my_colormaps import my_cmap_r
+from bs4 import BeautifulSoup
+import re
 # for graphviz
 #from sklearn.tree import export_text
 #from sklearn.utils import _pprint
@@ -115,7 +117,12 @@ def show():
     preprocessor = build_pipeline(df)
     set_config(display='diagram')
     html_code = estimator_html_repr(preprocessor)
-    st.components.v1.html(html_code, height=600, scrolling=True)
+    soup = BeautifulSoup(html_code, 'html.parser')
+    for rect in soup.find_all('rect'):
+        if rect.has_attr('style'):
+            rect['style'] = re.sub(r'fill:\s*#[0-9a-fA-F]{6}', 'fill:#097a80', rect['style'])  # Gold-Gelb
+    st.components.v1.html(str(soup), height=600, scrolling=True)
+    #st.components.v1.html(html_code, height=600, scrolling=True)
 #    try:
 #        dot_data = preprocessor._repr_svg_()  # Jupyter-interne SVG-Visualisierung (funktioniert teils in Streamlit)
 #        if dot_data:
