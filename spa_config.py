46a99cf89476d20771f8a46b92b0deb3ca378a69
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-07-02 21:14:46 +0200

    Create spa_config.py

---
+++ spa_config.py
@@ -0,0 +1,17 @@
+spa_plot_map = {
+    "Age Group": {
+        "feature": "age",
+        "bins": [15,25, 35, 45, 55,65,75,85,99],
+        "title": "Success Profile by Age Group"
+    },
+    "Employment Variation Rate": {
+        "feature": "emp.var.rate",
+        "bins": [-3.405, -2.2, -1.0, -0.1, 1.1, 1.4],
+        "title": "Success Profile for Economic Environment"
+    },
+    "Number of Employees": {
+        "feature": "nr.employed",
+        "bins": [4960.0, 5099.10, 5191.02, 5228.0 ,5300.0],
+        "title": "Success Profile for Employment Figures"
+    }
+}
