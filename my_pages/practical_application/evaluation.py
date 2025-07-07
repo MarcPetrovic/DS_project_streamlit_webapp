c9fd568ab1f0fb59c02956b8e55b0b2b3072ce46
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-07-07 21:07:54 +0200

    Update evaluation.py

--- my_pages/practical_application/evaluation.py
+++ my_pages/practical_application/evaluation.py
@@ -5,7 +5,7 @@ from utils.data_loader import load_csv_data
 import pandas as pd
 import numpy as np
 from utils.my_colormaps import my_cmap_r
-
+from utils.cost_calc import calculate_costs
 
 def show():
     st.header("evaluation with flexible CSV-Loading")
