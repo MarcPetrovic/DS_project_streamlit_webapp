ac3f8bc65fb3fcde3e99b6933f786ef26a8465ff
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-07-08 22:13:07 +0200

    Update compute_metrics.py

--- utils/compute_metrics.py
+++ utils/compute_metrics.py
@@ -2,9 +2,20 @@ from sklearn.metrics import (
     accuracy_score, precision_score, recall_score, f1_score,
     cohen_kappa_score, matthews_corrcoef, confusion_matrix
 )
-from utils.cost_calc import calculate_costs
+from utils.cost_calc import calculate_cost
 
 def compute_metrics(y_true, y_proba, threshold):
+    """
+    Calculation of different metrics based on given probabilities and a threshold.
+
+    Args:
+        y_true (array-like): empirical label.
+        y_proba (array-like): predicted probability for class 1.
+        threshold (float): threshold for binary classification.
+
+    Returns:
+        dict: Dictionary with relevant classification metrics.
+    """
     y_pred = (y_proba >= threshold).astype(int)
     cm = confusion_matrix(y_true, y_pred)
     TN, FP, FN, TP = cm.ravel()
@@ -19,5 +30,5 @@ def compute_metrics(y_true, y_proba, threshold):
         "Accuracy": accuracy_score(y_true, y_pred),
         "Cohen's Kappa": cohen_kappa_score(y_true, y_pred),
         "Matthews Corr. Coef.": matthews_corrcoef(y_true, y_pred),
-        "Total Cost (€)": calculate_costs(y_true, y_pred)
+        "Total Cost (€)": calculate_cost(y_true, y_pred)
     }
