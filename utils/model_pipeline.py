f6294ee306217db3e2599658002acce705fcbaef
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-07-09 22:19:15 +0200

    Update model_pipeline.py

--- utils/model_pipeline.py
+++ utils/model_pipeline.py
@@ -2,13 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from xgboost import XGBClassifier
 from utils.data_loader import load_csv_data  # Pfad ggf. anpassen
-from utils.modelling_feature_engineering import build_modelling_pipeline
 
-def make_streamlit_arrow_compatible(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.convert_dtypes()
-    for col in df.select_dtypes(include='object').columns:
-        df[col] = df[col].apply(lambda x: str(x) if not pd.isna(x) else "")
-    return df
 
 def load_data():
     """
@@ -18,10 +12,10 @@ def load_data():
         X_train, X_test, y_train, y_test: Pandas DataFrames/Series
     """
     # 1. input data (features)
-    #X_train = load_csv_data("data/X_train_transformed2nd.csv", ignore_index_column=False)
-    #X_test = load_csv_data("data/X_test_transformed2nd.csv", ignore_index_column=False)
-    X_train = load_csv_data("data/X_train.csv", ignore_index_column=False)
-    X_test = load_csv_data("data/X_test.csv", ignore_index_column=False)
+    X_train = load_csv_data("data/X_train_transformed2nd.csv", ignore_index_column=False)
+    X_test = load_csv_data("data/X_test_transformed2nd.csv", ignore_index_column=False)
+    #X_train = load_csv_data("data/X_train.csv", ignore_index_column=False)
+    #X_test = load_csv_data("data/X_test.csv", ignore_index_column=False)
     # 2. target feature (labels)
     y_train = load_csv_data("data/y_train.csv", ignore_index_column=True).squeeze()
     y_test = load_csv_data("data/y_test.csv", ignore_index_column=True).squeeze()
@@ -48,22 +42,6 @@ def train_and_predict(model_type='logistic'):
     """
     # 1. Loading data
     X_train, X_test, y_train, y_test = load_data()
-
-    processor = build_modelling_pipeline(X_train)  # Custom-built scikit-learn pipeline
-    processor.set_output(transform='pandas')
-    X_train_transformed2nd = processor.fit_transform(X_train)
-    X_test_transformed2nd = processor.transform(X_test)
-
-    X_train_transformed2nd = make_streamlit_arrow_compatible(X_train_transformed2nd)
-    X_test_transformed2nd = make_streamlit_arrow_compatible(X_test_transformed2nd)
-
-    from feature_engine.creation import CyclicalFeatures
-    cyclical_features = CyclicalFeatures(variables=['month_numeric', 'day_of_week_numeric'], 
-                                     drop_original=True)
-    X_train_transformed2nd = cyclical_features.fit_transform(X_train_transformed2nd)
-    X_test_transformed2nd = cyclical_features.transform(X_test_transformed2nd)
-    X_train_transformed2nd = make_streamlit_arrow_compatible(X_train_transformed2nd)
-    X_test_transformed2nd = make_streamlit_arrow_compatible(X_test_transformed2nd)    
 
     # 2. Select modell
     if model_type == 'logistic':
