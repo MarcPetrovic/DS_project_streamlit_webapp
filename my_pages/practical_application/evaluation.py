0bb17597237c311c3b95ac34937a2898ea58c99b
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-07-13 20:07:43 +0200

    Update evaluation.py

--- my_pages/practical_application/evaluation.py
+++ my_pages/practical_application/evaluation.py
@@ -51,64 +51,64 @@ def show():
         return logreg_model, xgb_model, X_train, y_train
 
     def render_html_table_metrics(df: pd.DataFrame) -> str:
-    html = """
-    <style>
-        .scrollable-table-container {
-            height: 400px;
-            overflow-y: auto;
-            border: 1px solid #ccc;
-            padding: 10px;
-        }
-
-        table {
-            width: 100% !important;
-            border-collapse: collapse !important;
-            table-layout: fixed !important;
-            border: 2px solid black !important;
-        }
-        th {
-            position: sticky;
-            top: 0;
-            background-color: #097a80 !important;
-            color: white !important;
-            border: 1px solid lightgray !important;
-            text-align: left !important;
-            padding: 8px !important;
-            word-break: break-word !important;
-            max-width: 250px !important;
-            font-size: 14px !important;
-        }
-        td {
-            background-color: white !important;
-            color: black !important;
-            border: 1px solid black !important;
-            text-align: left !important;
-            padding: 8px !important;
-            word-break: break-word !important;
-            max-width: 250px !important;
-            font-size: 14px !important;
-        }
-    </style>
-    <div class="scrollable-table-container">
-    <table>
-        <thead>
-            <tr>
-    """
-
-    for col in df.columns:
-        html += f"<th>{col}</th>"
-    html += "</tr></thead><tbody>"
-
-    for _, row in df.iterrows():
-        html += "<tr>"
-        for col in df.columns:
-            cell = row[col]
-            html += f"<td>{str(cell)}</td>"
-        html += "</tr>"
-
-    html += "</tbody></table></div>"
-    return html
+        html = """
+        <style>
+            .scrollable-table-container {
+                height: 400px;
+                overflow-y: auto;
+                border: 1px solid #ccc;
+                padding: 10px;
+            }
     
+            table {
+                width: 100% !important;
+                border-collapse: collapse !important;
+                table-layout: fixed !important;
+                border: 2px solid black !important;
+            }
+            th {
+                position: sticky;
+                top: 0;
+                background-color: #097a80 !important;
+                color: white !important;
+                border: 1px solid lightgray !important;
+                text-align: left !important;
+                padding: 8px !important;
+                word-break: break-word !important;
+                max-width: 250px !important;
+                font-size: 14px !important;
+            }
+            td {
+                background-color: white !important;
+                color: black !important;
+                border: 1px solid black !important;
+                text-align: left !important;
+                padding: 8px !important;
+                word-break: break-word !important;
+                max-width: 250px !important;
+                font-size: 14px !important;
+            }
+        </style>
+        <div class="scrollable-table-container">
+        <table>
+            <thead>
+                <tr>
+        """
+    
+        for col in df.columns:
+            html += f"<th>{col}</th>"
+        html += "</tr></thead><tbody>"
+    
+        for _, row in df.iterrows():
+            html += "<tr>"
+            for col in df.columns:
+                cell = row[col]
+                html += f"<td>{str(cell)}</td>"
+            html += "</tr>"
+    
+        html += "</tbody></table></div>"
+        return html
+        
     # 1. Einführung
     if strategy is None:
         st.markdown("""
