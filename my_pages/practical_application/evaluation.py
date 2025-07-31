dc4c2580c44a866fe7bf4eab2605223382d17308
MarcPetrovic <marc.petrovic@stud.uni-due.de>
2025-07-31 23:07:38 +0200

    Update evaluation.py

--- my_pages/practical_application/evaluation.py
+++ my_pages/practical_application/evaluation.py
@@ -667,7 +667,6 @@ def show():
                 summarized in Table 6 and visualized in Figures 46 and 47, which include the Confusion Matrix for both models.
                 """,
                 plot_figure=None,
-                markdown_after_plot,
                 y_test=y_test,
                 logreg_probs=logreg_probs,
                 xgb_probs=xgb_probs,
